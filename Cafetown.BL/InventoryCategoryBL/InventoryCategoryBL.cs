f15f301a8d6b711f83b3884d1761a0ff9968840c
Tr Thai Tuan <tuanlinhtx02@gmail.com>
2023-04-13 23:37:52 +0700

    add: inventoryCategoryBL

---
+++ Cafetown.BL/InventoryCategoryBL/InventoryCategoryBL.cs
@@ -0,0 +1,29 @@
+﻿using Cafetown.Common;
+using Cafetown.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafetown.BL
+{
+    public class InventoryCategoryBL : IInventoryCategoryBL
+    {
+        #region Field
+        private readonly IInventoryCategoryDL _inventoryCategoryDL;
+        #endregion
+
+        #region Constructor
+        public InventoryCategoryBL(IInventoryCategoryDL inventoryCategoryDL) 
+        {
+            _inventoryCategoryDL = inventoryCategoryDL;
+        }
+        #endregion
+
+        public IEnumerable<InventoryCategory> GetAllRecords()
+        {
+            return _inventoryCategoryDL.GetAllRecords();
+        }
+    }
+}
